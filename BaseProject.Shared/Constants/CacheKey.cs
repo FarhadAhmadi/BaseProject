6109b321e23bee6766fd1d299c6d2e0ec7914dc8
FarhadAhmadi <farhadahmadi1413@gmail.com>
2025-09-13 21:17:00 +0330

    update

--- BaseProject.Shared/Constants/CacheKey.cs
+++ BaseProject.Shared/Constants/CacheKey.cs
@@ -17,5 +17,6 @@ namespace BaseProject.Shared.Constants
         public const string PERMISSIONS_USER_OVERRIDE_ACTION_KEY = "permissions.overrideaction-{0}-{1}-{2}"; // userId, permission, action
 
         #endregion
+
     }
 }
